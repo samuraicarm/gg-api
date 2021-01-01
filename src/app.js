c640efc50e76220e6d35102e0b3b503fb25767b8
cd <carmendukes@gmail.com>
2021-01-01 12:36:48 -0800

    update to app js

--- src/app.js
+++ src/app.js
@@ -1,35 +1,46 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const { NODE_ENV } = require("./config");
 
-const app = express()
+const app = express();
 
-const morganOption = (NODE_ENV === 'production')
-    ? 'tiny'
-    : 'common';
+const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
-app.use(morgan(morganOption))
-app.use(helmet())
+app.use(morgan(morganOption));
+app.use(helmet());
 
+const games = require("./gamesdata.js");
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!')
-})
+/*app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});*/
 
+app.get("/games", (req, res) => {
+  res.send("Hello, world!");
+
+  res.json(games);
+});
+
+app.listen(8000, () => {
+  console.log("Server started on PORT 8000");
+});
+
+/*
 app.use(function errorHandler(error, req, res, next) {
-    let response
-    if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
-    } else {
-        console.error(error)
-        response = { message: error.message, error }
-    }
-    res.status(500).json(response)
-})
+  let response;
+  if (NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
+*/
 
-app.use(cors())
+app.use(cors());
 
-module.exports = app
+module.exports = app;
---
+++ src/gamesdata.js
@@ -0,0 +1,55 @@
+module.exports = [
+  {
+    id: 1,
+    name: "Among Trees",
+    status: "Play List",
+    img: "https://dummyimage.com/300",
+    platform: "PC",
+  },
+
+  {
+    id: 2,
+    name: "What Remains of Edith Finch",
+    status: "Played",
+    img: "https://dummyimage.com/300",
+    platform: "Switch",
+  },
+
+  {
+    id: 3,
+    name: "Zelda: Breath of the Wild",
+    status: "Play List",
+    img: "https://dummyimage.com/300",
+    platform: "Switch",
+  },
+  {
+    id: 4,
+    name: "The Gardens Between",
+    status: "Played",
+    img: "https://dummyimage.com/300",
+    platform: "iPhone",
+  },
+
+  {
+    id: 5,
+    name: "The Last Campfire",
+    status: "Play List",
+    img: "https://dummyimage.com/300",
+    platform: "Switch",
+  },
+
+  {
+    id: 6,
+    name: "CyberPunk2077",
+    status: "Play List",
+    img: "https://dummyimage.com/300",
+    platform: "Xbox",
+  },
+  {
+    id: 7,
+    name: "Call of Duty: Black Ops Cold War",
+    status: "Play List",
+    img: "https://dummyimage.com/300",
+    platform: "Playstation",
+  },
+];
