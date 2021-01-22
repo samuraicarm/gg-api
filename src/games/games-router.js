13e81762505d1c47c36ea2f2fd63ec4d8c65555b
cd <carmendukes@gmail.com>
2021-01-22 09:50:56 -0800

    update route

--- src/games/games-router.js
+++ src/games/games-router.js
@@ -1,13 +1,32 @@
 const express = require("express");
 const GamesService = require("./games-service");
-
+const axios = require("axios");
 const gamesRouter = express.Router();
 
-gamesRouter.route("/games").get((req, res) => {
+gamesRouter.route("/api/list").get((req, res) => {
   const knexInstance = req.app.get("db");
   GamesService.getAllGames(knexInstance).then((games) => {
     res.json(games);
   });
+});
+
+gamesRouter.route("/api/games").get((req, res) => {
+  const config = {
+    method: "get",
+    url: `https://api.igdb.com/v4/games?search=${req.query.search}&fields=id,name,cover`,
+    headers: {
+      "Client-ID": process.env.GAME_API_KEY,
+      Authorization: `Bearer ${process.env.GAME_API_TOKEN}`,
+    },
+  };
+
+  axios(config)
+    .then(function (response) {
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 });
 
 module.exports = gamesRouter;
